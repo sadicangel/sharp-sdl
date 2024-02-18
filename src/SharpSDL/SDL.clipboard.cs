fbe1f82447be0fb80c1f3717d526f277be8176fb
sadicangel <sadicangel@hotmail.com>
2024-02-18 18:10:14 +0000

    Adds bindings for clipboard and scancode.

---
+++ src/SharpSDL/SDL.clipboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace SharpSDL;
+internal static partial class SDL
+{
+    [LibraryImport(DllName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial SDL_bool SDL_HasClipboardText();
+
+    [LibraryImport(DllName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static unsafe partial byte* SDL_GetClipboardText();
+
+    [LibraryImport(DllName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static unsafe partial int SDL_SetClipboardText(byte* text);
+}
