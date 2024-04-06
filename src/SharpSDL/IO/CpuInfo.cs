e90a1cf54e637aeb2737112a0e9a310eb949b5e0
sadicangel <sadicangel@hotmail.com>
2024-04-06 01:10:06 +0100

    Adds CpuInfo wrapper.

---
+++ src/SharpSDL/IO/CpuInfo.cs
@@ -0,0 +1,41 @@
+﻿namespace SharpSDL.IO;
+public static class CpuInfo
+{
+    public static int Cores { get => SDL.GetCPUCount(); }
+
+    public static int L1CacheSize { get => SDL.GetCPUCacheLineSize(); }
+
+    public static bool HasRDTSC { get => SDL.HasRDTSC(); }
+
+    public static bool HasAltiVec { get => SDL.HasAltiVec(); }
+
+    public static bool HasMMX { get => SDL.HasMMX(); }
+
+    public static bool Has3DNow { get => SDL.Has3DNow(); }
+
+    public static bool HasSSE { get => SDL.HasSSE(); }
+
+    public static bool HasSSE2 { get => SDL.HasSSE2(); }
+
+    public static bool HasSSE3 { get => SDL.HasSSE3(); }
+
+    public static bool HasSSE41 { get => SDL.HasSSE41(); }
+
+    public static bool HasSSE42 { get => SDL.HasSSE42(); }
+
+    public static bool HasAVX { get => SDL.HasAVX(); }
+
+    public static bool HasAVX2 { get => SDL.HasAVX2(); }
+
+    public static bool HasAVX512F { get => SDL.HasAVX512F(); }
+
+    public static bool HasARMSIMD { get => SDL.HasARMSIMD(); }
+
+    public static bool HasNEON { get => SDL.HasNEON(); }
+
+    public static bool HasLSX { get => SDL.HasLSX(); }
+
+    public static bool HasLASX { get => SDL.HasLASX(); }
+
+    public static int SystemRamMiB { get => SDL.GetSystemRAM(); }
+}
---
+++ src/SharpSDL/IO/SimdMemoryManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Buffers;
+
+namespace SharpSDL.IO;
+
+public sealed class SimdMemoryManager(nuint length) : SimdMemoryManager<byte>(length)
+{
+    public static nuint Alignment { get => SDL.SIMDGetAlignment(); }
+}
+
+public class SimdMemoryManager<T> : MemoryManager<T> where T : unmanaged
+{
+    private readonly unsafe T* _pointer;
+    private readonly nuint _length;
+
+    public SimdMemoryManager(nuint length)
+    {
+        unsafe
+        {
+            _pointer = (T*)SDL.SIMDAlloc(length * (nuint)sizeof(T));
+            if (_pointer is null)
+                throw new SdlException("Out of memory");
+            _length = length;
+        }
+    }
+
+    public void Realloc(nuint length)
+    {
+        unsafe
+        {
+            var realloc = (T*)SDL.SIMDRealloc(_pointer, length * (nuint)sizeof(T));
+            if (realloc is null)
+                throw new SdlException("Out of memory");
+
+            fixed (T** p = &_pointer)
+                *p = realloc;
+            fixed (nuint* l = &_length)
+                *l = length;
+        }
+    }
+
+    public sealed override Span<T> GetSpan()
+    {
+        unsafe { return new Span<T>(_pointer, (int)_length); }
+    }
+
+    public sealed override MemoryHandle Pin(int elementIndex = 0)
+    {
+        if (elementIndex < 0 || (nuint)elementIndex >= _length)
+            throw new ArgumentOutOfRangeException(nameof(elementIndex));
+        unsafe { return new MemoryHandle(_pointer + elementIndex); }
+    }
+
+    public sealed override void Unpin()
+    {
+        // This performs no operation since the underlying memory is unmanaged.
+    }
+
+    protected sealed override unsafe void Dispose(bool disposing)
+    {
+        if (_pointer is not null)
+        {
+            SDL.SIMDFree(_pointer);
+            fixed (T** ptr = &_pointer)
+                *ptr = null;
+        }
+    }
+}
