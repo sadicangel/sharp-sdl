f7a6f896cb73ac7ca989e4251c5fb723dbfc6493
sadicangel <sadicangel@hotmail.com>
2024-04-06 01:10:13 +0100

    Adds clipboard wrapper.

---
+++ src/SharpSDL/IO/Clipboard.cs
@@ -0,0 +1,76 @@
+﻿using System.Buffers;
+using System.Text;
+
+namespace SharpSDL.IO;
+public static class Clipboard
+{
+    public static bool HasText { get => SDL.HasClipboardText(); }
+
+    public static string Text
+    {
+        get
+        {
+            unsafe
+            {
+                using var pointer = SdlPointer.Defer(SDL.GetClipboardText());
+                var span = MemoryMarshal.CreateReadOnlySpanFromNullTerminated(pointer);
+                if (span.Length == 0)
+                    SdlException.ThrowLastError();
+                return Encoding.UTF8.GetString(span);
+            }
+        }
+        set
+        {
+            unsafe
+            {
+                var utf8 = ArrayPool<byte>.Shared.Rent(Encoding.UTF8.GetMaxByteCount(value.Length));
+                try
+                {
+                    var bytesWritten = Encoding.UTF8.GetBytes(value, utf8);
+                    fixed (byte* p = utf8.AsSpan(..bytesWritten))
+                        if (SDL.SetClipboardText(p) is not 0)
+                            SdlException.ThrowLastError();
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(utf8, clearArray: true);
+                }
+            }
+        }
+    }
+
+    public static bool HasPrimarySelectionText { get => SDL.HasPrimarySelectionText(); }
+
+    public static string PrimarySelectionText
+    {
+        get
+        {
+            unsafe
+            {
+                using var pointer = SdlPointer.Defer(SDL.GetPrimarySelectionText());
+                var span = MemoryMarshal.CreateReadOnlySpanFromNullTerminated(pointer);
+                if (span.Length == 0)
+                    SdlException.ThrowLastError();
+                return Encoding.UTF8.GetString(span);
+            }
+        }
+        set
+        {
+            unsafe
+            {
+                var utf8 = ArrayPool<byte>.Shared.Rent(Encoding.UTF8.GetMaxByteCount(value.Length));
+                try
+                {
+                    var bytesWritten = Encoding.UTF8.GetBytes(value, utf8);
+                    fixed (byte* p = utf8.AsSpan(..bytesWritten))
+                        if (SDL.SetPrimarySelectionText(p) is not 0)
+                            SdlException.ThrowLastError();
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(utf8, clearArray: true);
+                }
+            }
+        }
+    }
+}
